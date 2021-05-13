07befdf837e6c65ddb154600b5c8569abcb3629e
heamanth-11 <80298864+heamanth-11@users.noreply.github.com>
2021-05-13 17:17:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var audio = new Audio("sounds/tom-1.mp3");
+
+for( var i =0;i<document.querySelectorAll(".drum").length;i++ ){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    this.style.color="white";
+     audio.play();
+    
+  });
+
+}
