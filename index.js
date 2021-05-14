216f5ab6a61e7e95ce8950a95761b204e0d9be47
heamanth-11 <80298864+heamanth-11@users.noreply.github.com>
2021-05-14 15:43:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-var audio4 = new Audio("sounds/tom-1.mp3");
-var audio1 = new Audio("sounds/crash.mp3");
-var audio2 = new Audio("sounds/kick-bass.mp3");
-var audio3 = new Audio("sounds/tom-2.mp3");
-var audio5 = new Audio("sounds/tom-3.mp3");
-var audio6 = new Audio("sounds/tom-4.mp3");
-var audio7 = new Audio("sounds/snare.mp3")
-
-
-for( var i =0;i<document.querySelectorAll(".drum").length;i++ ){
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-  
-    switch (this.innerHTML) {
-      case "w":
-        audio1.play();
-        break;
-      case "a":
-        audio2.play();
-        break;
-      case "s":
-        audio3.play();
-        break;
-      case "d":
-        audio4.play();
-        break;
-      case "j":
-        audio5.play();
-        break;
-      case "l":
-        audio6.play();
-        break;
-      case "k":
-        audio7.play();
-        break;
-      default:
-        break;
-    }
-  });
+    makesound(this.innerHTML);
+  }
+  );
+}
+document.addEventListener("keydown", function (event) {
+  makesound(event.key);
+})
 
+function makesound(key) {
+  
+  
+  switch (key) {
+    case "w":
+      var audio1 = new Audio("sounds/crash.mp3");
+      audio1.play();
+      break;
+    case "a":
+      var audio2 = new Audio("sounds/kick-bass.mp3");
+      audio2.play();
+      break;
+    case "s":
+      var audio3 = new Audio("sounds/tom-2.mp3");
+      audio3.play();
+      break;
+    case "d":
+      var audio4 = new Audio("sounds/tom-1.mp3");
+      audio4.play();
+      break;
+    case "j":
+      var audio5 = new Audio("sounds/tom-3.mp3");
+      audio5.play();
+      break;
+    case "l":
+      var audio6 = new Audio("sounds/tom-4.mp3");
+      audio6.play();
+      break;
+    case "k":
+      var audio7 = new Audio("sounds/snare.mp3");
+      audio7.play();
+      break;
+    default:
+      break;
+  }
 }
